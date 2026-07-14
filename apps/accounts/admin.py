d10a9c8643fbbc60aa00f3f1ad5d47c5164ea56f
Lee Kaburu <leekaburu@gmail.com>
2026-07-14 07:47:26 +0300

    feat(accounts): Added some fieldsets to the adminpanel

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (
+            "Verification",
+            {
+                "fields": (
+                    "is_email_verified",
+                    "otp_code_hash",
+                    "otp_expires_at",
+                )
+            },
+        ),
+    )
+    list_display = ("username", "email", "is_email_verified", "is_staff")
+
+
+admin.site.register(User, UserAdmin)
--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+# username, password, first_name, last_name, is_staff, is_active, is_superuser, last_login, date_joined imported from AbstractUser
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     otp_code_hash = models.CharField(max_length=255, null=True, blank=True)
